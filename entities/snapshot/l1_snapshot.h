fd2dd383bf5937d10b814106b53b5fe0173dbedd
Evgenii Kazakov <evgenstf@gmail.com>
2019-11-23 21:44:30 +0100

    [backend][entities] add l1 snapshot

---
+++ entities/snapshot/l1_snapshot.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "../direction/direction.h"
+
+namespace market::entities {
+
+class L1OrderBookSnapshot {
+public:
+  struct Quote {
+    double price;
+    uint64_t amount;
+  };
+
+  L1Snapshot(Quote best_bid, Quote best_ask):
+    best_bid_(std::move(best_bid)), best_ask_(std::move(best_ask)) {}
+
+  const Quote& best_quote(Direction direction) {
+    return (direction == Direction::Bid) ? best_bid_ : best_ask_;
+  }
+
+private:
+  Quote best_bid_;
+  Quote best_ask_;
+};
+
+}  // namespace market::entities
