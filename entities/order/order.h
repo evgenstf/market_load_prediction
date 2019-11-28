a557efcc07c233b2fc86a4e7dec3579f93a57180
Evgenii Kazakov <evgenstf@gmail.com>
2019-11-28 17:21:29 +0100

    [backend][entities] add direction to order

--- entities/order/order.h
+++ entities/order/order.h
@@ -1,6 +1,13 @@
 #pragma once
+#include "../direction/direction.h"
 #include <string>
 #include <cassert>
+
+namespace market::matching_engine {
+
+class MatchingEngine;
+
+}  // namespace market::matching_engine
 
 namespace market::entities {
 
@@ -11,22 +18,30 @@ public:
     Ioc = 1
   };
 
-  Order(double price, uint64_t amount, Type type, std::string uuid = ""):
-      price_(price), amount_(amount), type_(type), uuid_(std::move(uuid)) {
+  Order(double price, uint64_t amount, Direction direction, Type type, std::string uuid = ""):
+      price_(price), amount_(amount), direction_(direction), type_(type), uuid_(std::move(uuid)) {
     assert(price_ > 0);
     assert(amount_ > 0);
   }
 
   double price() const { return price_; }
   uint64_t amount() const { return amount_; }
+  Direction direction() const { return direction_; }
   Type type() const { return type_; }
   const std::string& uuid() const { return uuid_; }
 
 private:
+  void set_amount(uint64_t amount) {
+    amount_ = amount;
+  }
+
   double price_;
   uint64_t amount_;
+  Direction direction_;
   Type type_;
   std::string uuid_;
+
+  friend class market::matching_engine::MatchingEngine;
 };
 
 }  // namespace market::entities
